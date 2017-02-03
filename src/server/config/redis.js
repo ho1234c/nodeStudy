44e73759b32b9b56be6647365f0c5228346fd57f
jongho <ho1234c@gmail.com>
2017-02-03 13:33:19 +0900

    modified diployment setting

--- src/server/config/redis.js
+++ src/server/config/redis.js
@@ -4,20 +4,23 @@ import connectRedis from 'connect-redis'
 import config from '.';
 
 export default function(app) {
+    const host = config.db.redis.REDIS_HOST;
+    const port = config.db.redis.PORT;
+
     const redisStore = connectRedis(session);
-    const redisClient = redis.createClient();
+    const redisClient = redis.createClient(port, host);
 
     redisClient.on('connect', () => {
         console.log('Running Redis successfully');
-    }).on('error', () => {
-        console.log('Fail to start Redis');
+    }).on('error', (err) => {
+        console.log(err);
     });
 
     app.use(session({
-        secret: config.db.redis.secretKey,
+        secret: config.db.redis.REDIS_SECRETKEY,
         store: new redisStore({
-            host: config.db.redis.host,
-            port: config.db.redis.port,
+            host: host,
+            port: port,
             client: redisClient,
         }),
         saveUninitialized: false,
