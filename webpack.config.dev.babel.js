6c9b1b6fb1a05ef8ac0421d3cc54ce720451ec49
JONGHO-SURFACEB\jongho <ho1234c@gmail.com>
2017-03-02 20:17:19 +0900

    Created webpack config file for dev mode

--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -3,7 +3,7 @@ import ProgressBarPlugin from 'progress-bar-webpack-plugin';
 import path from 'path';
 
 export default {
-    devtool: (() => { return 'source-map' })(),
+    devtool: (() => { return 'eval' })(),
     entry: './src/client/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -14,8 +14,15 @@ export default {
             path.resolve('./node_modules'),
         ]
     },
-    module: {
-        loaders: [
+    module:{
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "jshint-loader",
+            }
+        ],
+        loaders:[
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -24,7 +31,7 @@ export default {
             },
             {
                 test: /\.scss$/,
-                loaders: ['style', 'css', 'sass']
+                loaders: [ 'style', 'css', 'sass' ]
             },
             {
                 test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
@@ -32,19 +39,10 @@ export default {
             }
         ]
     },
-    plugins: (() => {
+    plugins:(() => {
         const plugins = [];
 
-        plugins.push(
-            new webpack.optimize.UglifyJsPlugin({
-                compressor: {
-                    warnings: false
-                },
-                mangle: false
-            })
-        );
         plugins.push(new ProgressBarPlugin(), new webpack.OldWatchingPlugin());
-
         return plugins;
     })()
 };
